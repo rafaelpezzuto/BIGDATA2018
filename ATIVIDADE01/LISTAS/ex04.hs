85b17051b9ad668865dd7c15b62e896ba11e6b0b
Rafael Pezzuto <rafael.pezzuto@gmail.com>
2018-03-18 14:08:23 -0300

    ex04 listas corrigido - atividade01 finalizada

--- ATIVIDADE01/LISTAS/ex04.hs
+++ ATIVIDADE01/LISTAS/ex04.hs
@@ -12,17 +12,18 @@ isPar x
     | x `rem` 2 == 0 = True
     | otherwise = False
 
+isLessOrEqualThan4000000 x
+    | x <= 4000000 = True
+    | otherwise = False
+
 fib = 1 : 2 : prox fib
     where
         prox (x : t@(y:z)) = (x+y) : prox t
 
+projectoEuler2 = sum(takeWhile (isLessOrEqualThan4000000) [x | x <- fib, isPar x] `using` parListChunk 50000 rseq)
+
 main :: IO()
 main = do
-    print (sum([x | x <- fib, isPar x, x <= 400] `using` parListChunk 10000 rseq))
+    print (projectoEuler2)
 
 -- soma resultou 4613732
--- incompleto
--- mudar estrategia para, caso lista fib chegue a um valor maior que 4000000
---   parar geracao da lista
---   filtrar impares
---   realizar soma da lista resultante (pares menores ou iguais a 4000000)
